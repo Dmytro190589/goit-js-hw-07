5e8741c89e5acee466895368958d29d55ecda20d
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-10-26 17:06:58 +0300

    7-1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const creatCardContainer = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
+
+creatCardContainer.insertAdjacentHTML('beforeend',cardsMarkup)
+function createCardsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+        }).join('');
+}
+creatCardContainer.addEventListener('click',onClickCards);
+
+function onClickCards(evt){
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')){
+        return;
+    }
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="auto" height="auto">
+`)
+
+instance.show()
+creatCardContainer.addEventListener("keydown", e =>{
+    if(e.code === "Escape"){
+        instance.close();
+    }
+})
+}
+
