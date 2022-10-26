bbe667a03240effd1c229c7ed044486d59b32042
Dmytro Kharchenko <dmytro190589@gmail.com>
2022-10-26 19:00:34 +0300

    7 таск

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,11 +3,11 @@ import { galleryItems } from './gallery-items.js';
 const creatCardContainer = document.querySelector('.gallery');
 const cardsMarkup = createCardsMarkup(galleryItems);
 
-creatCardContainer.insertAdjacentHTML('beforeend',cardsMarkup)
-function createCardsMarkup(galleryItems) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-            return `<div class="gallery__item">
+creatCardContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+function createCardsMarkup(cards) {
+  return cards
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
@@ -17,24 +17,25 @@ function createCardsMarkup(galleryItems) {
           />
         </a>
       </div>`;
-        }).join('');
+    }).join('');
 }
-creatCardContainer.addEventListener('click',onClickCards);
+creatCardContainer.addEventListener('click', onClickCards);
 
-function onClickCards(evt){
-    evt.preventDefault();
-    if (!evt.target.classList.contains('gallery__image')){
-        return;
-    }
+function onClickCards(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
   const instance = basicLightbox.create(`
     <img src="${evt.target.dataset.source}" width="auto" height="auto">
 `)
 
-instance.show()
-creatCardContainer.addEventListener("keydown", e =>{
-    if(e.code === "Escape"){
-        instance.close();
+  instance.show()
+  creatCardContainer.addEventListener("keydown", e => {
+    if (e.code === "Escape") {
+      instance.close();
     }
-})
+  })
 }
 
+console.log(galleryItems);
